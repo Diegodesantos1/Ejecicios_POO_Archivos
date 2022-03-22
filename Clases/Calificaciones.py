eced26f73f70383545b9be71c1f7b8e27b91059a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-23 00:17:15 +0100

    actualizados los porcentajes de la asistencia

--- Clases/Calificaciones.py
+++ Clases/Calificaciones.py
@@ -40,7 +40,7 @@ class Calificaciones_estudiantes:
             print(lista_inicial)
         elegir_subejercicio()
     def aprobado_suspenso():
-        lista_de_listas = [["Anido Bonet", "David", "90%", "5.5", "2.75", "0", "5", "0", "0"],["Bueno Cerdeira", "Patricia", "95%", "9.5", "9", "0", "0", "6.25", "0"],["Casariego García", "Raúl", "98%", "4.25", "5.75", "0", "0", "4", "5"],["Curbelo Sánchez", "Jorge", "75%", "6.75", "4.25", "0", "0", "0", "6.5"],["Díaz Souto", "Sofía", "82%", "7", "6.5", "0", "0", "9", "0"],["García Perez", "Yaiza", "85%", "10", "8", "0", "0", "7.5", "0"],["Sánchez Jordán", "María", "92%", "8.75", "9", "0", "0", "6.5", "0"],["Lorenzo García", "Jaime", "100%", "9.75", "8.25", "0", "0", "7.5", "0"],["Martínez Lucas", "Cecilia", "86%", "7", "4", "0", "0", "6.25", "0"],["Mora Peñaloza", "Sandra", "70%", "5.25", "2", "0", "4", "6.5", "0"],["Morillo Escudero", "Ana", "100%", "9.5", "10", "0", "0", "8.75", "0"],["Muñoz Gómez", "Carolina", "94%", "7.75", "6.5", "0", "0", "4", "0"],["Ramirez de la Puente", "Raquel", "75%", "0", "1", "1", "2.75", "2.25", "3.25"],["Riego Pizarro", "Carlos", "75%", "4", "2.5", "5", "3.75", "0", "0"],["Rodríguez de Blas", "Ignacio", "80%", "8.25", "5.25", "0", "0", "6.5", "0"],["Moreno Angulo", "Antonio", "88%", "9", "6.75", "0", "0", "5.25", "0"]]
+        lista_de_listas = [["Anido Bonet", "David", "0.9", "5.5", "2.75", "0", "5", "0", "0"],["Bueno Cerdeira", "Patricia", "0.95", "9.5", "9", "0", "0", "6.25", "0"],["Casariego García", "Raúl", "0.98", "4.25", "5.75", "0", "0", "4", "5"],["Curbelo Sánchez", "Jorge", "0.75", "6.75", "4.25", "0", "0", "0", "6.5"],["Díaz Souto", "Sofía", "0.82", "7", "6.5", "0", "0", "9", "0"],["García Perez", "Yaiza", "0.85", "10", "8", "0", "0", "7.5", "0"],["Sánchez Jordán", "María", "0.92", "8.75", "9", "0", "0", "6.5", "0"],["Lorenzo García", "Jaime", "1", "9.75", "8.25", "0", "0", "7.5", "0"],["Martínez Lucas", "Cecilia", "0.86", "7", "4", "0", "0", "6.25", "0"],["Mora Peñaloza", "Sandra", "0.7", "5.25", "2", "0", "4", "6.5", "0"],["Morillo Escudero", "Ana", "1", "9.5", "10", "0", "0", "8.75", "0"],["Muñoz Gómez", "Carolina", "0.94", "7.75", "6.5", "0", "0", "4", "0"],["Ramirez de la Puente", "Raquel", "0.75", "0", "1", "1", "2.75", "2.25", "3.25"],["Riego Pizarro", "Carlos", "0.75", "4", "2.5", "5", "3.75", "0", "0"],["Rodríguez de Blas", "Ignacio", "0.8", "8.25", "5.25", "0", "0", "6.5", "0"],["Moreno Angulo", "Antonio", "0.88", "9", "6.75", "0", "0", "5.25", "0"]]
         aprobados= []
         suspensos=[]
         while len(lista_de_listas) != 0:
@@ -49,7 +49,7 @@ class Calificaciones_estudiantes:
             parcial1 = float(parcial1) ; parcial2 = float(parcial2) ; practicas = float(practicas)
             nota_final = ((parcial1 * 0.3) + (parcial2 * 0.3)) + (practicas * 0.4)
             nota_final =round(nota_final)
-            if asistencia > "75%" and nota_final >= 5 and practicas >= 4:
+            if asistencia > "0.75" and nota_final >= 5 and practicas >= 4:
                 nombre = lista_inicial.pop(1)
                 apellidos = lista_inicial.pop(0)
                 alumno=f"{nombre} {apellidos}"
@@ -59,7 +59,7 @@ class Calificaciones_estudiantes:
                 apellidos = lista_inicial.pop(0)
                 alumno=f"{nombre} {apellidos}"
                 suspensos.append(alumno)
-        print(f"Los alumnos que han aprobado son:\n")
+        print(f"\nLos alumnos que han aprobado son:\n")
         for x in aprobados:
             print(Fore.GREEN + x)
         print(Fore.WHITE + "\nLos alumnos que han suspendido son:\n")
