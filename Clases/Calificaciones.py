1a0612a78e4057fbb9ab272abc794f1782f5989c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 19:48:51 +0100

    planetado casi todo del if

--- Clases/Calificaciones.py
+++ Clases/Calificaciones.py
@@ -43,11 +43,15 @@ class Calificaciones_estudiantes:
         lista_de_listas = [["Anido Bonet", "David", "90%", "5.5", "2.75", "0", "5", "0", "0"],["Bueno Cerdeira", "Patricia", "95%", "9.5", "9", "0", "0", "6.25", "0"],["Casariego García", "Raúl", "98%", "4.25", "5.75", "0", "0", "4", "5"],["Curbelo Sánchez", "Jorge", "75%", "6.75", "4.25", "0", "0", "0", "6.5"],["Díaz Souto", "Sofía", "82%", "7", "6.5", "0", "0", "9", "0"],["García Perez", "Yaiza", "85%", "10", "8", "0", "0", "7.5", "0"],["Sánchez Jordán", "María", "92%", "8.75", "9", "0", "0", "6.5", "0"],["Lorenzo García", "Jaime", "100%", "9.75", "8.25", "0", "0", "7.5", "0"],["Martínez Lucas", "Cecilia", "86%", "7", "4", "0", "0", "6.25", "0"],["Mora Peñaloza", "Sandra", "70%", "5.25", "2", "0", "4", "6.5", "0"],["Morillo Escudero", "Ana", "100%", "9.5", "10", "0", "0", "8.75", "0"],["Muñoz Gómez", "Carolina", "94%", "7.75", "6.5", "0", "0", "4", "0"],["Ramirez de la Puente", "Raquel", "75%", "0", "1", "1", "2.75", "2.25", "3.25"],["Riego Pizarro", "Carlos", "75%", "4", "2.5", "5", "3.75", "0", "0"],["Rodríguez de Blas", "Ignacio", "80%", "8.25", "5.25", "0", "0", "6.5", "0"],["Moreno Angulo", "Antonio", "88%", "9", "6.75", "0", "0", "5.25", "0"]]
         while len(lista_de_listas) != 0:
             lista_inicial=lista_de_listas.pop(0)
-            parcial1= (lista_inicial.pop(3)) ; parcial2 = (lista_inicial.pop(3)) ; final = (lista_inicial.pop(5)) ; asistencia = (lista_inicial.pop(2))
-            parcial1 = float(parcial1) ; parcial2 = float(parcial2) ; final = float(final)
+            parcial1= (lista_inicial.pop(3)) ; parcial2 = (lista_inicial.pop(3)) ; final = (lista_inicial.pop(5)) ; asistencia = (lista_inicial.pop(2)) ; practicas = (lista_inicial.pop(7))
+            parcial1 = float(parcial1) ; parcial2 = float(parcial2) ; final = float(final) ; practicas = float(practicas)
             nota_final = ((parcial1 * 0.3) + (parcial2 * 0.3)) + (final * 0.4)
             nota_final =round(nota_final)
-            if asistencia > "75%" and nota_final > 5 and nota_exam_prac > 4:
+            if asistencia > "75%" and nota_final >= 5 and practicas >= 4:
+                nombre = lista_inicial.pop(1)
+                apellidos = lista_inicial.pop(2)
+                pass
+            else:
                 pass
 
 
