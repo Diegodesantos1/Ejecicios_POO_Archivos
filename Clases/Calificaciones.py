173c321ad9ed3ac925356a75fe699fce0a18aa4b
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 15:43:11 +0100

    puesto el separador y lee la tabla perfectamente

--- Clases/Calificaciones.py
+++ Clases/Calificaciones.py
@@ -4,7 +4,7 @@ class Calificaciones_estudiantes:
     def __init__(self, lista):
         self.lista = lista
     def recibir_diccionario():
-        diccionario = pd.read_csv('calificaciones.csv', on_bad_lines='skip', encoding = "UTF8")
-        print(diccionario)
+        tabla = pd.read_csv('calificaciones.csv', on_bad_lines='skip', encoding = "UTF8", sep=';')
+        print(tabla)
 
 Calificaciones_estudiantes.recibir_diccionario()
