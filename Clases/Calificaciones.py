526d84afb375b4a6831ff5e2a51ddc3f9d975348
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 19:39:57 +0100

    reutilizo código de la otra función

--- Clases/Calificaciones.py
+++ Clases/Calificaciones.py
@@ -41,6 +41,15 @@ class Calificaciones_estudiantes:
         elegir_subejercicio()
     def aprobado_suspenso():
         lista_de_listas = [["Anido Bonet", "David", "90%", "5.5", "2.75", "0", "5", "0", "0"],["Bueno Cerdeira", "Patricia", "95%", "9.5", "9", "0", "0", "6.25", "0"],["Casariego García", "Raúl", "98%", "4.25", "5.75", "0", "0", "4", "5"],["Curbelo Sánchez", "Jorge", "75%", "6.75", "4.25", "0", "0", "0", "6.5"],["Díaz Souto", "Sofía", "82%", "7", "6.5", "0", "0", "9", "0"],["García Perez", "Yaiza", "85%", "10", "8", "0", "0", "7.5", "0"],["Sánchez Jordán", "María", "92%", "8.75", "9", "0", "0", "6.5", "0"],["Lorenzo García", "Jaime", "100%", "9.75", "8.25", "0", "0", "7.5", "0"],["Martínez Lucas", "Cecilia", "86%", "7", "4", "0", "0", "6.25", "0"],["Mora Peñaloza", "Sandra", "70%", "5.25", "2", "0", "4", "6.5", "0"],["Morillo Escudero", "Ana", "100%", "9.5", "10", "0", "0", "8.75", "0"],["Muñoz Gómez", "Carolina", "94%", "7.75", "6.5", "0", "0", "4", "0"],["Ramirez de la Puente", "Raquel", "75%", "0", "1", "1", "2.75", "2.25", "3.25"],["Riego Pizarro", "Carlos", "75%", "4", "2.5", "5", "3.75", "0", "0"],["Rodríguez de Blas", "Ignacio", "80%", "8.25", "5.25", "0", "0", "6.5", "0"],["Moreno Angulo", "Antonio", "88%", "9", "6.75", "0", "0", "5.25", "0"]]
+        while len(lista_de_listas) != 0:
+            lista_inicial=lista_de_listas.pop(0)
+            parcial1= (lista_inicial.pop(3)) ; parcial2 = (lista_inicial.pop(3)) ; final = (lista_inicial.pop(5))
+            parcial1 = float(parcial1) ; parcial2 = float(parcial2) ; final = float(final)
+            nota_final = ((parcial1 * 0.3) + (parcial2 * 0.3)) + (final * 0.4)
+            lista_inicial.insert(3,parcial1) ; lista_inicial.insert(3,parcial2) ; lista_inicial.insert(5,final)
+            nota_final =round(nota_final)
+            lista_inicial.append(nota_final)
+
 
 def elegir_subejercicio():
     print (Fore.LIGHTMAGENTA_EX + "¿Qué enunciado quieres ejecutar?(1-3) o 4 para terminar el programa\n") ; print(Style.RESET_ALL, end="")
@@ -50,7 +59,7 @@ def elegir_subejercicio():
     elif enunciado == 2:
         Calificaciones_estudiantes.nota_final()
     elif enunciado == 3:
-        pass
+        Calificaciones_estudiantes.aprobado_suspenso()
     elif enunciado == 4:
         exit()
     else:
