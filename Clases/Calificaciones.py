7a2915002cb70e65665902c50978fc7bde56a8e0
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 14:44:08 +0100

    creada función recibir_diccionario

--- Clases/Calificaciones.py
+++ Clases/Calificaciones.py
@@ -1,3 +1,6 @@
 class Calificaciones_estudiantes:
-    def __init__(self, alumnos):
+    def __init__(self, alumnos, lista):
         self.alumnos = alumnos
+        self.lista = lista
+    def recibir_diccionario():
+        lista = []
