5551c7b88e67c1d1b1838c355aa779e060e5aed5
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 14:40:32 +0100

    creado main

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def eleccion_ejercicio():
+    variable = int(input("Seleccione que ejercicio desea ejecutar: \n -->1: Calificaciones\n"))
+    if variable == 1:
+        from Clases import Calificaciones
+    else:
+        eleccion_ejercicio()
+
+eleccion_ejercicio()
