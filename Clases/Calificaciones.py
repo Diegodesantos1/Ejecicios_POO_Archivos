31aa1674090e78ce6a06c5af074248554b75d153
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 14:54:40 +0100

    printea repetidos varios datos por ahora

--- Clases/Calificaciones.py
+++ Clases/Calificaciones.py
@@ -1,6 +1,13 @@
+import csv
 class Calificaciones_estudiantes:
     def __init__(self, alumnos, lista):
         self.alumnos = alumnos
         self.lista = lista
     def recibir_diccionario():
         lista = []
+        with open('calificaciones.csv') as File:
+            reader = csv.DictReader(File)
+            for row in reader:
+                lista.append(row)
+            print(lista)
+Calificaciones_estudiantes.recibir_diccionario()
