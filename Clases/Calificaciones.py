021806a551b3ee0044b03a3cfc1bba1938c537cd
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-25 22:16:26 +0100

    arreglado el ejercicio apartado 3

--- Clases/Calificaciones.py
+++ Clases/Calificaciones.py
@@ -50,24 +50,21 @@ class Calificaciones_estudiantes:
         print(f"\nLos datos añadida la nota final son : \n\n\n{diccionario}")
         elegir_subejercicio()
     def aprobado_suspenso():
-        lista_de_listas = [["Anido Bonet", "David", "0.9", "5.5", "2.75", "0", "5", "0", "0"],["Bueno Cerdeira", "Patricia", "0.95", "9.5", "9", "0", "0", "6.25", "0"],["Casariego García", "Raúl", "0.98", "4.25", "5.75", "0", "0", "4", "5"],["Curbelo Sánchez", "Jorge", "0.75", "6.75", "4.25", "0", "0", "0", "6.5"],["Díaz Souto", "Sofía", "0.82", "7", "6.5", "0", "0", "9", "0"],["García Perez", "Yaiza", "0.85", "10", "8", "0", "0", "7.5", "0"],["Sánchez Jordán", "María", "0.92", "8.75", "9", "0", "0", "6.5", "0"],["Lorenzo García", "Jaime", "1", "9.75", "8.25", "0", "0", "7.5", "0"],["Martínez Lucas", "Cecilia", "0.86", "7", "4", "0", "0", "6.25", "0"],["Mora Peñaloza", "Sandra", "0.7", "5.25", "2", "0", "4", "6.5", "0"],["Morillo Escudero", "Ana", "1", "9.5", "10", "0", "0", "8.75", "0"],["Muñoz Gómez", "Carolina", "0.94", "7.75", "6.5", "0", "0", "4", "0"],["Ramirez de la Puente", "Raquel", "0.75", "0", "1", "1", "2.75", "2.25", "3.25"],["Riego Pizarro", "Carlos", "0.75", "4", "2.5", "5", "3.75", "0", "0"],["Rodríguez de Blas", "Ignacio", "0.8", "8.25", "5.25", "0", "0", "6.5", "0"],["Moreno Angulo", "Antonio", "0.88", "9", "6.75", "0", "0", "5.25", "0"]]
-        aprobados= []
-        suspensos=[]
-        while len(lista_de_listas) != 0:
-            lista_inicial=lista_de_listas.pop(0)
-            asistencia = (lista_inicial.pop(2)) ; parcial1= (lista_inicial.pop(2)) ; parcial2 = (lista_inicial.pop(2)) ; practicas = (lista_inicial.pop(4))
-            parcial1 = float(parcial1) ; parcial2 = float(parcial2) ; practicas = float(practicas)
+        aprobados = [] ; suspensos = []
+        datos = pd.read_csv("calificaciones.csv", header=0 , sep =";")
+        lista_apellidos = list(datos["Apellidos"]) ; lista_nombre = list(datos["Nombre"]) ; lista_asistencia = list(datos["Asistencia"])
+        lista_parcial1 = list(datos["Parcial1"]) ; lista_parcial2 = list(datos["Parcial2"]) ; lista_ordinario1 = list(datos["Ordinario1"])
+        lista_ordinario2 = list(datos["Ordinario2"]) ; lista_practicas = list(datos["Practicas"]) ; lista_ordinariopracticas = list(datos["OrdinarioPracticas"])
+        for i in range (16):
+            apellido=lista_apellidos.pop(0) ; nombre=lista_nombre.pop(0) ; asistencia = lista_asistencia.pop(0)
+            parcial1=lista_parcial1.pop(0) ; parcial2=lista_parcial2.pop(0) ; ordinario1 = lista_ordinario1.pop(0)
+            ordinario2 = lista_ordinario2.pop(0) ; practicas = lista_practicas.pop(0) ; ordinariopracticas = lista_ordinariopracticas.pop(0)
             nota_final = ((parcial1 * 0.3) + (parcial2 * 0.3)) + (practicas * 0.4)
-            nota_final =round(nota_final)
             if asistencia >= "0.75" and nota_final >= 5 and practicas >= 4:
-                nombre = lista_inicial.pop(1)
-                apellidos = lista_inicial.pop(0)
-                alumno=f"{nombre} {apellidos}"
+                alumno=f"{nombre} {apellido}"
                 aprobados.append(alumno)
             else:
-                nombre = lista_inicial.pop(1)
-                apellidos = lista_inicial.pop(0)
-                alumno=f"{nombre} {apellidos}"
+                alumno=f"{nombre} {apellido}"
                 suspensos.append(alumno)
         print(f"\nLos alumnos que han aprobado son:\n")
         for x in aprobados:
